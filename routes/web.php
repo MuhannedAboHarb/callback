bb4a54941f29b71894b1e0416c1aa66b3b8097ea
MuhannedAboHarb <muhannedprof@gmail.com>
2023-07-27 23:26:27 +0300

    use Packge Breeze and make page Authentication

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::group([
     'prefix'=>'/dashboard',
     'as'=>'dashboard.',
 //     'namespace'=>'Dashboard'
+     'middleware'=>['auth']   
 ] , function() {
        Route::get('/',[DashboardController::class,'index']);  
 
@@ -81,3 +82,15 @@ Route::group([
        });
 
 });
+
+Route::get('/dashboard/breeze', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
