d4e90c1a69e91963229e23c6030f7d85d09a875a
MuhannedAboHarb <muhannedprof@gmail.com>
2023-09-02 20:01:56 +0300

    finish realtion one to one -> one user has one profile page

--- app/Http/Controllers/Auth/UserProfileController.php
+++ app/Http/Controllers/Auth/UserProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
@@ -13,6 +14,8 @@ class UserProfileController extends Controller
     public function index()
     {
         $user = Auth::user();
+        //$profile = $user->profile;
+
         return view( 'auth.user-profile' ,[
             'user' => $user
         ]);
@@ -24,15 +27,30 @@ class UserProfileController extends Controller
         $user = $request->user();
 
         $request->validate([
-            'name' => ['required' , 'string' , 'max:255'] ,
+            'first_name' => ['required' , 'string' , 'max:100'] ,
+            'last_name' => ['required' , 'string' , 'max:255'] ,
+            'name' => ['required' , 'string' , 'max:100'] ,
             'email' => [
                 'required',
                 'email',
                 Rule::unique('users')->ignore($user->id)
-            ]
+            ] , 
+
+            'birthday' => 'date|before:today' , 
+            'gender' => 'in:male,female'
         ]);
         
         $user->update($request->all());
+
+        $profile =  $user->profile;
+        $request->merge([
+            'user_id' => $user->id
+        ]);
+        if(!$profile->exists) {
+            $profile = Profile::create($request->all());
+        } else {
+            $profile->update($request->all());
+        }
 
         return redirect()
         ->route('profile')
--- resources/views/auth/user-profile.blade.php
+++ resources/views/auth/user-profile.blade.php
@@ -15,12 +15,97 @@
         @csrf
         @method('patch')
 
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.input name="first_name" :value=" $user->profile->first_name " lable="First Name" />    
+                </div>        
+            </div>
+
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.input name="last_name" :value=" $user->profile->last_name " lable="Last Name" />    
+                </div>   
+            </div>
+        </div>
+
         <div class="form-group">
-            <x-form.input name="name" :value=" $user->name " lable="Name" />    
+            <x-form.input name="name" :value=" $user->name " lable="Display Name" />    
         </div>
 
         <div class="form-group">
             <x-form.input name="email" :value=" $user->email " lable="Email Address" />
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.input type="date"  name="birthday" :value=" $user->profile->birthday " lable="Birthday" />    
+                </div>        
+            </div>
+
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.lable>Gender</x-form.lable>
+                    <div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="gender" id="male" value="male" @if($user->profile->gender == 'male') checked @endif>
+                            <label class="form-check-label" for="male">Male</label>
+                        </div>
+
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="gender" id="female" value="female" @if($user->profile->gender == 'female') checked @endif>
+                            <label class="form-check-label" for="female">Female</label>
+                        </div>
+                    </div>    
+                </div>   
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.input  name="city" :value=" $user->profile->city " lable="City" />    
+                </div>        
+            </div>
+
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.lable>Country</x-form.lable>
+                    <select name="country_code" id="country_code" class="form-control">
+                        <option value="">Select Country</option>
+                        @foreach (Symfony\Component\Intl\Countries::getNames() as $code => $name )
+                            <option value="{{ $code }}" @if($user->profile->country_code == $code) selected @endif>{{ $name }}</option>
+                        @endforeach
+                    </select>
+                </div>   
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.lable>Language</x-form.lable>  {{-- مقصود فيه ال لوكل --}}
+                    <select name="locale" id="locale" class="form-control">
+                        <option value="">Select Language</option>
+                        @foreach (Symfony\Component\Intl\Locales::getNames() as $code => $name )
+                            <option value="{{ $code }}" @if($user->profile->locale == $code) selected @endif>{{ $name }}</option>
+                        @endforeach
+                    </select>
+                </div>           
+            </div>
+
+            <div class="col-6">
+                <div class="form-group">
+                    <x-form.lable>Timezone</x-form.lable>
+                    <select name="timezone" id="timezone" class="form-control">
+                        <option value="">Select Timezone</option>
+                        @foreach (Symfony\Component\Intl\Timezones::getNames() as $code => $name )
+                            <option value="{{ $code }}" @if($user->profile->timezone == $code) selected @endif>{{ $name }}</option>
+                        @endforeach
+                    </select>
+                </div>   
+            </div>
         </div>
 
         <div class="form-group">
