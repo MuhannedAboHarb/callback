794886b152b78600cc6b20bbadf5c5c3e1494a04
MuhannedAboHarb <muhannedprof@gmail.com>
2024-03-27 19:39:33 +0200

    make all broadcast notification real time

--- resources/views/components/notifications-menu.blade.php
+++ resources/views/components/notifications-menu.blade.php
@@ -1,6 +1,5 @@
 <a class="nav-link" data-toggle="dropdown" href="#">
     <i class="far fa-bell"></i>
-    <span class="badge badge-warning navbar-badge">{{ $unread }}</span>
     <span class="badge badge-warning navbar-badge" id="unread-count">{{ $unread }}</span>
 </a>
 <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -10,11 +9,11 @@
         @foreach ($notifications as $notification)
             <a href="{{ route('dashboard.notifications.read', $notification->id) }}" class="dropdown-item">
                 @if ($notification->unread())
-                    unread
+                    <i class="fas fa-envelope mr-2"></i>
+                @else
                     <i class="fas fa-envelope-open mr-2"></i>
                 @endif
                 {{ $notification->data['title'] }}
-                <span class="float-right text-muted text-sm">{{ $notification->created_at->diffForHumans() }}</span>
                 <span
                     class="float-right text-muted text-sm">{{ $notification->created_at->diffForHumans('now', true, true) }}</span>
             </a>
